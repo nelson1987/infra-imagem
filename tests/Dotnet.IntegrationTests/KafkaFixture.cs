b05f551f0a1c6700412014aaf8d0d44c5a1ed41b
Nelson Neto <nelson.ash@outlook.com>
2023-12-24 10:12:28 -0300

    supress

--- tests/Dotnet.IntegrationTests/KafkaFixture.cs
+++ tests/Dotnet.IntegrationTests/KafkaFixture.cs
@@ -7,30 +7,40 @@ namespace Dotnet.IntegrationTests;
 
 public static class KafkaFixture
 {
+#pragma warning disable CS0649 // Campo "KafkaFixture.EventClient" nunca é atribuído e sempre terá seu valor padrão null
     private static readonly IEventClient? EventClient;
+#pragma warning restore CS0649 // Campo "KafkaFixture.EventClient" nunca é atribuído e sempre terá seu valor padrão null
     public static void ConfigureKafkaServices(IServiceCollection services)
+#pragma warning disable CS8634 // O tipo não pode ser usado como parâmetro de tipo no tipo ou método genérico. A anulabilidade do argumento de tipo não corresponde à restrição 'class'.
+#pragma warning disable CS8621 // A nulidade de tipos de referência no tipo de retorno não corresponde ao delegado de destino (possivelmente devido a atributos de nulidade).
         => services
             //.RemoveAll<KafkaHealthCheck>()
             .RemoveAll<IEventClient>()
             //.RemoveAll<IEventClientConsumers>()
             //.RemoveAll<IEventClientProducer>()
             .AddSingleton(_ => EventClient);
+#pragma warning restore CS8621 // A nulidade de tipos de referência no tipo de retorno não corresponde ao delegado de destino (possivelmente devido a atributos de nulidade).
+#pragma warning restore CS8634 // O tipo não pode ser usado como parâmetro de tipo no tipo ou método genérico. A anulabilidade do argumento de tipo não corresponde à restrição 'class'.
     //.AddSingleton<IEventClientConsumers>(_ => EventClientConsumers)
     //.AddSingleton<IEventClientProducer>(_ => EventClientProducer);
     public static Task Produce<T>(T data)
     {
         using var cancellationToken = ExpiringCancellationToken();
         AddTeacherCreatedClientEvent? evento = data as AddTeacherCreatedClientEvent;
+#pragma warning disable CS8602 // Desreferência de uma referência possivelmente nula.
         return EventClient.Produce(
                 evento,
                 cancellationToken.Token);
+#pragma warning restore CS8602 // Desreferência de uma referência possivelmente nula.
     }
     public static T Consume<T>(string topic, int timeout = 150)
     {
         try
         {
             using var cancellationToken = ExpiringCancellationToken(timeout);
+#pragma warning disable CS8602 // Desreferência de uma referência possivelmente nula.
             return EventClient.Consume(topic, JsonSerializer.Deserialize<T>, cancellationToken.Token);
+#pragma warning restore CS8602 // Desreferência de uma referência possivelmente nula.
         }
         catch { return default!; }
     }
